6d61c2ed947e0e0e599422d9805ace5b0f31cbb1
nuzulzen <nuzulzen@gmail.com>
2020-08-13 22:25:10 +0700

    add fix navbar and responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,27 @@
-// ---navbar responsive---
-const navSlide = () => {
-      const burger = document.querySelector('.burger');
-      const nav = document.querySelector('.menu-nav');
-
-      burger.addEventListener('click' ,()=>{
-        nav.classList.toggle('nav-active');
+//---navbar---
+const navSlide = ()=>{
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.menu-nav');
+  const navLinks = document.querySelectorAll('.menu-nav li');
+  
+  burger.addEventListener('click', () => {
+      //toggle Nav
+      nav.classList.toggle('nav-active');
+      //animate links
+      navLinks.forEach((link, index) => {
+        if(link.style.animation) {
+          link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5}s`
+          }
       });
+      //burger animation
+      burger.classList.toggle('toggle');
+
+  });
+ 
 }
+
 navSlide();
 
 // ---form js---
